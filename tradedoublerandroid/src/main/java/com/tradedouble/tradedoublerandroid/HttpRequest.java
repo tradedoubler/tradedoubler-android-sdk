80c47c8f731bd16c1c9d68f144e42a64139ca13a
Magdalena Dziesinska <magdalenadziesinska@gmail.com>
2020-11-12 10:02:26 +0100

    add  reqest

--- tradedoublerandroid/src/main/java/com/tradedouble/tradedoublerandroid/HttpRequest.java
+++ tradedoublerandroid/src/main/java/com/tradedouble/tradedoublerandroid/HttpRequest.java
@@ -1,8 +1,30 @@
 package com.tradedouble.tradedoublerandroid;
 
+import com.tradedouble.tradedoublerandroid.utils.Constans;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+
+import static com.tradedouble.tradedoublerandroid.utils.Constans.EVENT;
+import static com.tradedouble.tradedoublerandroid.utils.Constans.EXT_ID;
+import static com.tradedouble.tradedoublerandroid.utils.Constans.EXT_TYP;
+import static com.tradedouble.tradedoublerandroid.utils.Constans.ORGANIZATION_ID;
+import static com.tradedouble.tradedoublerandroid.utils.Constans.LEAD_NUMBER;
+import static com.tradedouble.tradedoublerandroid.utils.Constans.TDUID;
+
 public class HttpRequest {
 
 
+    public String trackingLead(String organizationId, String leadEventId, String leadId, String tduid, String extId) {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constans.BASE_URL_SALE).newBuilder();
+        urlBuilder.addQueryParameter(ORGANIZATION_ID, organizationId);
+        urlBuilder.addQueryParameter(EVENT, leadEventId);
+        urlBuilder.addQueryParameter(LEAD_NUMBER, leadId);
+        urlBuilder.addQueryParameter(TDUID, tduid);
+        urlBuilder.addQueryParameter(EXT_TYP, "1");
+        urlBuilder.addQueryParameter(EXT_ID, extId);
+        return urlBuilder.toString();
+    }
 
 
 }
--- tradedoublerandroid/src/main/java/com/tradedouble/tradedoublerandroid/utils/Constans.java
+++ tradedoublerandroid/src/main/java/com/tradedouble/tradedoublerandroid/utils/Constans.java
@@ -1,6 +1,13 @@
 package com.tradedouble.tradedoublerandroid.utils;
 
 public class Constans {
-
-    public static String BASE_URL = "";
+    public static String BASE_URL = "http://tbl.tradedoubler.com/report";
+    public static String BASE_URL_SALE = "http://tbs.tradedoubler.com/report";
+    public static String ORGANIZATION_ID = "organization";
+    public static String EVENT_ID = "event";
+    public static String LEAD_NUMBER = "leadNumber";
+    public static String TDUID = "tduid";
+    public static String EXT_TYP = "exttype";
+    public static String EXT_ID = "extid";
+    public static String EVENT = "event";
 }
